3a32edacfbb8171e1169980bdb664c0d6bfb1aaa
250542018-netizen <250542018@firat.edu.tr>
2025-10-19 12:13:01 +0300

    Update BesKereTekrar.java

---
+++ Task-1-HosgeldinMesajlari/BesKereTekrar.java
@@ -0,0 +1,13 @@
+ static void main() {
+        System.out.println("Java ’ya Hos Geldiniz ");
+        System.out.println("Java ’ya Hos Geldiniz ");
+        System.out.println("Java ’ya Hos Geldiniz ");
+        System.out.println("Java ’ya Hos Geldiniz ");
+        System.out.println("Java ’ya Hos Geldiniz ");
+        
+        System.out.println("--------------------------");
+        
+        for (int i=0; i<5; i++) {
+            System.out.println("Java ’ya Hos Geldiniz ");
+        }
+    }
