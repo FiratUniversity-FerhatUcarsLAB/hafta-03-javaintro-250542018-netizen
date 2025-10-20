8810784f061e0decf7489852450a70bc0b9ec08a
250542018-netizen <250542018@firat.edu.tr>
2025-10-20 21:50:50 +0300

    Update IkiIslem.java

---
+++ Task-2-Matematikselislemler/IkiIslem.java
@@ -0,0 +1,10 @@
+public class Matematikselİfadeler {
+    public static void main ( String [] args ) {
+         double pay = (9.5*4.5) - (2.5*3) ;
+         double payda = 45.5 - 3.5;
+         double son;
+          son = pay / payda ;
+         System . out . print ("%.4 f\n"+ son ) ;
+         }
+
+}
