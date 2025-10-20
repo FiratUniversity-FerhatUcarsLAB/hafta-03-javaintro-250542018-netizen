d1705b53c67c23c1990239eb4ab0e0b8953c3a30
250542018-netizen <250542018@firat.edu.tr>
2025-10-20 22:45:29 +0300

    Update DikdortgenHesap.java

---
+++ Task-3-GeometrikHesaplamalar/DikdortgenHesap.java
@@ -0,0 +1,16 @@
+public class AlanCevre {
+    public static void main(String[] args) {
+
+         System.out.println("--------------------------");
+
+         double en=4.5;
+         double boy=7.9;
+         double alan, cevre;
+         alan=en*boy;
+         cevre=(en+boy)*2;
+         System.out.println("Dikdörgenin Alanı: "+alan);
+        System.out.println("Dikdörgenin Çevresi: "+cevre);
+
+
+    }
+}
