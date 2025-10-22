63f09f127392ff72d1c9f4bdbdc98e1c4314d07c
250542018-netizen <250542018@firat.edu.tr>
2025-10-22 22:50:06 +0300

    Update UsluSayilarTablosu.java

---
+++ Task-5-TabloDesenleri/UsluSayilarTablosu.java
@@ -0,0 +1,7 @@
+   public static void main(String[] args) {
+                System.out.println("a\ta^2\ta^3");
+                System.out.println("---------------------------");
+                for (int a = 1; a <= 5; a++) {
+                    System.out.println(a + "\t" + (a * a) + "\t" + (a * a * a));
+                }
+    }
