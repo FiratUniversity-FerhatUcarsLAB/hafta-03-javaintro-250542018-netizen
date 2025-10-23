e451eca32ff6c90c8eba95620679e71a59829ca7
250542018-netizen <250542018@firat.edu.tr>
2025-10-23 12:11:34 +0300

    Update AsciiArtIsim.java

---
+++ TASK-BONUS/AsciiArtIsim.java
@@ -0,0 +1,9 @@
+ public static void main(String[] args) {
+        System.out.println("         _        _     _        _      _   _ ");
+        System.out.println("        / \\      | |  / /       | |    | \\ | |");
+        System.out.println("       / _ \\     | |_/ /        | |    |  \\| |");
+        System.out.println("      / ___ \\    |  _  |        | |    | . ` |");
+        System.out.println("     / /   \\ \\   | | \\ \\        | |    | |\\  |");
+        System.out.println("    /_/     \\_\\  |_|  \\_\\       |_|    |_| \\_|");
+        
+    }
